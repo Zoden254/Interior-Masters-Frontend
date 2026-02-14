1b0b84b5e2bbf0927968b25d25ab728b447c50f9
Zoden254 <deejayzoden@gmail.com>
2026-02-14 12:22:41 +0300

    tailwind commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunc() {
+        document.getElementById("greeting").style.backgroundColor = "pink";
+        document.getElementById("button1").innerHTML = '35px';
+        window.print()
+    }
